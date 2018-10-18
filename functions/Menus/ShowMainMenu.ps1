9a5b44243714f00c1cfbacefe6f2da55b80234df
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-10-18 09:51:47 -0700

    remove obsolete entries from menu
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- functions/Menus/ShowMainMenu.ps1
+++ functions/Menus/ShowMainMenu.ps1
@@ -51,21 +51,15 @@ function ShowMainMenu() {
 
         Write-Host "------ Infrastructure -------"
         Write-Host "1: Configure existing Azure Kubernetes Service"
-        Write-Host "2: Launch Kubernetes Dashboard"
         Write-Host "3: Launch AKS Dashboard"
         Write-Host "------ Troubleshooting Infrastructure -------"
-        Write-Host "5: Start VMs in Resource Group"
-        Write-Host "6: Stop VMs in Resource Group"
-
         #    Write-Host "3: Launch Traefik Dashboard"
         Write-Host "9: Show nodes"
         Write-Host "10: Show DNS entries for /etc/hosts"
 
         Write-Host "----- Troubleshooting ----"
         Write-Host "20: Show status of cluster"
-        Write-Host "22: Show SSH commands to VMs"
         Write-Host "23: View status of DNS pods"
-        Write-Host "24: Restart all VMs"
 
         Write-Host "------ Keyvault -------"
         Write-Host "26: Copy Kubernetes secrets to keyvault"
@@ -73,18 +67,9 @@ function ShowMainMenu() {
 
         Write-Host "------ Load Balancer -------"
         Write-Host "30: Test load balancer"
-        Write-Host "31: Fix load balancers"
-        Write-Host "32: Redeploy load balancers"
-        Write-Host "33: Launch Load Balancer Dashboard"
-
-        Write-Host "------- Troubleshooting ----"
-        Write-Host "50: Troubleshooting Menu"
 
         Write-Host "------ Realtime -------"
         Write-Host "52: Fabric.Realtime Menu"
-
-        Write-Host "------ Older Scripts -------"
-        Write-Host "100: Go to old menu"
 
         Write-Host "q: Quit"
         #--------------------------------------
@@ -106,21 +91,6 @@ function ShowMainMenu() {
                 }
                 LaunchAksDashboard -resourceGroup $resourceGroup
             }
-            '5' {
-                Do {
-                    $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group"
-                }
-                while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
-
-                StartVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP
-            }
-            '6' {
-                Do {
-                    $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group"
-                }
-                while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
-                StopVMsInResourceGroup -resourceGroup $AKS_PERS_RESOURCE_GROUP
-            }
             '9' {
                 Write-Host "Current cluster: $(kubectl config current-context)"
                 kubectl version --short
@@ -139,14 +109,8 @@ function ShowMainMenu() {
                 kubectl version --short
                 kubectl get "deployments,pods,services,ingress,secrets,nodes" --namespace=kube-system -o wide
             }
-            '22' {
-                ShowSSHCommandsToVMs
-            }
             '23' {
                 RestartDNSPodsIfNeeded
-            }
-            '24' {
-                RestartAzureVMsInResourceGroup
             }
             '26' {
                 $currentResourceGroup = ReadSecretData -secretname azure-secret -valueName resourcegroup -Verbose
@@ -159,32 +123,6 @@ function ShowMainMenu() {
             '30' {
                 TestAzureLoadBalancer
             }
-            '31' {
-                $DEFAULT_RESOURCE_GROUP = ReadSecretData -secretname azure-secret -valueName resourcegroup
-
-                if ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP)) {
-                    Do {
-                        $AKS_PERS_RESOURCE_GROUP = Read-Host "Resource Group: (default: $DEFAULT_RESOURCE_GROUP)"
-                        if ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP)) {
-                            $AKS_PERS_RESOURCE_GROUP = $DEFAULT_RESOURCE_GROUP
-                        }
-                    }
-                    while ([string]::IsNullOrWhiteSpace($AKS_PERS_RESOURCE_GROUP))
-                }
-                FixLoadBalancers -resourceGroup $AKS_PERS_RESOURCE_GROUP
-            }
-            '32' {
-                $config = $(ReadConfigFile).Config
-                Write-Host $config
-
-                $local = $false
-                SetupLoadBalancer -baseUrl $baseUrl -config $config -local $local
-            }
-            '33' {
-                $loadBalancerInfo = $(GetLoadBalancerIPs)
-                $loadBalancerInternalIP = $loadBalancerInfo.InternalIP
-                LaunchTraefikDashboard -internalIp $loadBalancerInternalIP
-            }
             '50' {
                 ShowTroubleshootingMenu -baseUrl $baseUrl -local $local
                 $skip = $true
@@ -192,19 +130,6 @@ function ShowMainMenu() {
             '52' {
                 ShowRealtimeMenu -baseUrl $baseUrl -namespace "fabricrealtime" -local $local
                 $skip = $true
-            }
-            '100' {
-                # curl -useb https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/azure/main.ps1 | iex;
-                # $Script = Invoke-WebRequest -useb ${GITHUB_URL}/azure/main.ps1?f=$randomstring;
-                # $ScriptBlock = [Scriptblock]::Create($Script.Content)
-                # Invoke-Command -ScriptBlock $ScriptBlock
-                $scriptPath = "curl -useb https://raw.githubusercontent.com/HealthCatalyst/dos.install/master/azure/main.ps1 | iex;"
-                # $argumentList = ""
-                # Invoke-Expression "$scriptPath $argumentList"
-                # Invoke-Expression 'cmd /c start powershell -Command { $scriptPath $argumentList }'
-                # Start-Process powershell -Command "$scriptPath"
-                start-process powershell.exe -argument "-noexit -nologo -command $scriptPath"
-                exit 0
             }
             'q' {
                 return
