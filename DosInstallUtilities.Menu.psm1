9a74999bfc193321e1a21ef3e3076ee538bf9adc
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-11-15 10:00:31 -0800

    remove ShowProductMenu
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- DosInstallUtilities.Menu.psm1
+++ DosInstallUtilities.Menu.psm1
@@ -2,5 +2,4 @@
 
 # Menus
 . $PSScriptRoot\functions\Menus\ShowMainMenu.ps1
-. $PSScriptRoot\functions\Menus\ShowProductMenu.ps1
 . $PSScriptRoot\functions\Menus\ShowTroubleshootingMenu.ps1
--- runtests.ps1
+++ runtests.ps1
@@ -32,7 +32,6 @@ Import-Module "$here\..\$module\$module.psm1" -Force
 # Invoke-Pester "$here\functions\Storage\SetStorageAccountNameIntoSecret.Tests.ps1" -Tag 'Unit'
 # Invoke-Pester "$here\functions\Storage\SetStorageAccountNameIntoSecret.Tests.ps1" -Tag 'Integration'
 # Invoke-Pester "$here\functions\Storage\CreateAzureStorage.Tests.ps1" -Tag 'Integration' -Verbose
-# Invoke-Pester "$here\functions\Storage\InstallStackInKubernetes.Tests.ps1" -Tag 'Integration' -Verbose
 
 # # Subscription
 # Invoke-Pester "$here\functions\Subscription\SetCurrentAzureSubscription.Tests.ps1" -Tag 'Unit'
